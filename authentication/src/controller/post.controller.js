0510055486cad914158d882b4f69ad085c6a6b7b
Shubhamvumap123 <94427228+Shubhamvumap123@users.noreply.github.com>
2022-03-27 15:30:18 +0530

    done

--- authentication/src/controller/post.controller.js
+++ authentication/src/controller/post.controller.js
@@ -2,6 +2,7 @@ const express = require('express');
 const post = require("../model/post.model")
 const router = express.Router();
 
+
 router.post("",async(req,res)=>{
     try {
         req.body.user_id = req.user_id;
@@ -12,6 +13,7 @@ router.post("",async(req,res)=>{
     }
 });
 
+
 router.get("",async(req,res) =>{
     try {
         const post = await post.find().lean().exec();
@@ -20,5 +22,16 @@ router.get("",async(req,res) =>{
         return res.status(500).send({message:error.message})
     }
 });
+//nihal code for authorised
+router.patch("/:id", authenticate, authorise(["admin","seller"]), async(req, res) => {
+    try{
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true})
+        return res.status(200).send(product)
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+})
+
 
  module.exports=router;
